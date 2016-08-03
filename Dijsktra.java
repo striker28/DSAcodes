6f574ac7cc884d9d3ece2a665f1376c5c0b5c466
Ayush Sharma <f2014074@goa.bits-pilani.ac.in>
2016-08-04 00:36:01 +0530

    Update Dijsktra.java

--- Dijsktra.java
+++ Dijsktra.java
@@ -1,83 +1,134 @@
-import java.io.*;
-import java.util.*;
+package algos;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+/*
+ * Input Sample
+5 6
+0 1 4
+0 2 4
+0 3 5
+1 2 2
+2 4 1
+3 4 1
+
+5 6
+0 1 1
+0 2 4
+0 3 5
+1 2 2
+2 4 1
+3 4 1
+ */
+class Dijkstra {
+	
+	private ArrayList<ArrayList<Pair>> al = new ArrayList<>();
+	private PriorityQueue<Pair> queue = new PriorityQueue<>();
+	private Boolean[] visited;
+	private int[] dist;
+	static Scanner sc = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		
+		Dijkstra dObject = new Dijkstra();
+		
+		dObject.initialize(n,m);
+		dObject.doDijkstra();
+		dObject.printResult();
+		sc.close();
+	}
+	
+	private void printResult() {
+//		System.out.println("printing ans-----");
+		for(int d : dist){
+			System.out.println(d);
+		}
+		
+	}
+
+	private void initialize(int n, int m) {
+		for(int i=0;i<n;i++){
+			al.add(new ArrayList<Pair>());
+		}
+	
+		
+		for(int i=0;i<m;i++){
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			int w = sc.nextInt();
+			
+			al.get(a).add(new Pair(b,w));
+			al.get(b).add(new Pair(a,w));
+			
+		}
+	
+		visited = new Boolean[n];
+		for(int i=0;i<n;i++){
+			visited[i]=false;
+		}
+		
+		dist = new int[n];
+		for(int i=0;i<n;i++){
+			dist[i]=Integer.MAX_VALUE;
+		}
+		
+		dist[0]=0;
+		queue.add(new Pair(0,0));
+		
+	}
+
+	private void doDijkstra(){
+		while(!queue.isEmpty()){
+			Pair current = queue.poll();
+			Iterator<Pair> itr = al.get(current.getV()).iterator();
+		
+			while(itr.hasNext()){
+				Pair a = itr.next();
+				int x = dist[current.getV()] + a.getW();
+				if(x<dist[a.getV()]){
+					dist[a.getV()]=x;
+					queue.add(new Pair(a.getV(),x));
+				}
+			}
+		}
+	}
+}
+
 class Pair implements Comparable{
-    private int w;  // distance
-    private int v;  // vertex 1
-    public Pair(int w, int v) {
-        this.w = w;
-        this.v = v;
-    }
-    public int compareTo(Object o){
-        return this.w - ((Pair)o).w;
-    }
-    public int getw() {
-        return w;
-    }
-    public void setw(int w) {
-        this.w = w;
-    }
-    public int getv() {
-        return v;
-    }
-    public void setv(int v) {
-        this.v = v;
-    }
+	
+	private int v;
+	private int w;
+	
+	public Pair(int v, int w){
+		this.v=v;
+		this.w=w;
+	}
+	public int getV() {
+		return v;
+	}
+
+	public void setV(int v) {
+		this.v = v;
+	}
+
+	public int getW() {
+		return w;
+	}
+
+	public void setW(int w) {
+		this.w = w;
+	}
+	@Override
+	public int compareTo(Object o) {
+		Pair p = (Pair)o;
+		return this.w - p.getW();
+	}
+
 }
-
-
-public class Dijsktra{
-    private int V;   // No. of vertices
-    private LinkedList<Pair> adj[]; // Adjacency List Represntation
-    Dijsktra(int v) {
-        V = v;
-        adj = new LinkedList[V];
-        for(int i=0; i<v; ++i)
-            adj[i] = new LinkedList<Pair>();
-    }
-    void addEdge(int v,int w, int z) {
-        adj[v].add(new Pair(z,w));
-        adj[w].add(new Pair(z,v));
-    }
-    public static void main(String args[])
-    {
-        int from,to,weight;
-        int start,end;
-        Scanner in = new Scanner(System.in);
-        int n=in.nextInt();
-        int m=in.nextInt();
-        Dijsktra g1 = new Dikjstra(n);
-        int dist[] = new int[n];
-        for(int i=0;i<m;i++){
-            from=in.nextInt();
-            to=in.nextInt();
-            weight=in.nextInt(); 
-            g1.addEdge(from,to,weight);
-        }
-        start = in.nextInt();
-       // end = in.nextInt();
-        for (int i=0;i<n;i++){
-            dist[i] = Integer.MAX_VALUE;
-        }
-        PriorityQueue<Pair> pq=new PriorityQueue(); 
-        pq.add(new Pair(0,start));
-        dist[start] = 0; // Distance From Start To Itself Is 0
-
-        while (!pq.isEmpty()){
-        	Pair current = pq.poll();
-                System.out.println("Going to vertex: "+ current.getv());
-        	Iterator<Pair> itr = g1.adj[current.getv()].iterator();
-        	while (itr.hasNext()){
-        		Pair a = itr.next();
-        		int x = dist[current.getv()] + a.getw();
-        		if (x < dist[a.getv()]) {
-        			dist[a.getv()] = x;
-        			pq.add(new Pair(x,a.getv()));
-                                System.out.println("Updating: "+ " vertex: "+ a.getv() + " distance: "+ x +"  ");
-                        }
-        	}
-        }
-        for (int i=0;i<n;i++){	System.out.println("Distance To "+ i +" is "+dist[i]);}
-    }
-}
-
 
